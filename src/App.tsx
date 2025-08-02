68a5cfc5964ae118b62a94681cb14f5a92582241
leafboo <janjancaranguian@gmail.com>
2025-08-02 10:27:31 +0800

    Clear the selected papers when user searches for new topic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import List from './List'
 import Button from './Button'
 import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 type SelectedPaper = {
   id: string
@@ -15,6 +15,11 @@ export default function App() {
   const { register, handleSubmit } = useForm()
   const [searchQuery, setSearchQuery] = useState()
   const [selectedPapers, setSelectedPapers] = useState<SelectedPaper[]>([]) // <SelectedPaper[]> means the variable only accepts an array of objects with type 'SelectedPaper'
+
+  // if user searches for a new topic, clear current selected Papers (papers with checkboxes)
+  useEffect(() => {
+    setSelectedPapers([]);
+  }, [searchQuery]);
 
 
   function updateSelectedPapers(obj: SelectedPaper, isChecked: boolean) {
@@ -41,17 +46,18 @@ export default function App() {
         <div>
           <div className='flex justify-center'>
             <form onSubmit={handleSubmit((data) => {
-              setSearchQuery(data.searchQuery)
+              setSearchQuery(data.searchQuery);
             })}>
               <input {...register("searchQuery")} type="text" placeholder='Search for a topic' className='border-2 h-[2.5rem] w-[20rem] pl-2' required />
               <button className='bg-blue-400 text-white p-2.5 ml-6 duration-[0.15s] hover:bg-blue-700 cursor-pointer'>Search</button>
             </form>
           </div>
           <List query={searchQuery ?? null} updateSelectedPapers={updateSelectedPapers} />
+          <div className='mt-[2rem]'>Papers selected: {selectedPapers.length === 0 ? "0" : selectedPapers.length.toString()}</div>
+
           {
             selectedPapers.length !== 0 ? <Button /> : ""
           }
-          <div className='mt-[2rem]'>Papers selected: {selectedPapers.length === 0 ? "0" : selectedPapers.length.toString()}</div>
         </div>
       </div>
       
